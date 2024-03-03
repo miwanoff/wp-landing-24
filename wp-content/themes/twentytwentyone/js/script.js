15224111879130227216534ea017f4f135f3bb20
miwanoff <marina.iva.noffa@gmail.com>
2024-03-03 14:41:16 +0200

    Fix css & custom js

--- wp-content/themes/twentytwentyone/js/script.js
+++ wp-content/themes/twentytwentyone/js/script.js
@@ -1,13 +1,17 @@
 (function ($, undefined) {
-    $(function () {
-      $("h2").css("color", "red");
+  $(window).scroll(function () {
+    /* Скроллировать фоновые слои с разной скоростью */
+    let winTop = $(window).scrollTop();
+    let pos = "center " + (-90 - winTop * 0.5) + "px";
+    $(".mars").css("background-position", pos);
+
+    $(".mov_slideInRight").each(function () {
+      // для кожного блоку с класом mov_slideInRight
+      var pos = $(this).offset().top; // зчитуємо його координату по осі Y у вікні браузера
+      if (pos < $(window).scrollTop() + 600) {
+        //якщо до верху сторінки залишається 600px,
+        $(this).addClass("animate__animated animate__slideInRight"); // додаємо до блоку клас з анімацією slideInRight
+      }
     });
-    $(window).scroll(function () {
-      /* Скролювати фонові шари з різною швидкістю */
-      var winTop = $(window).scrollTop();
-      $(".home .mars").css(
-        "background-position",
-        "center" + -(winTop * 0.5) + "px"
-      );
-    });
-  }) (jQuery);
+  });
+})(jQuery);
